672f93f15eaed5bd8386c8e1a61808c88e390a87
SergeyOleks <oleksienkosa@gmail.com>
2023-06-22 17:55:38 +0300

    fix

--- src/Compoinents/ContactForm/ContactForm.js
+++ src/Compoinents/ContactForm/ContactForm.js
@@ -39,7 +39,7 @@ class ContactForm extends Component {
             onChange={this.handleNameChange}
             type="text"
             name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
